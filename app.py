54c2bcee6194282c28be39ef381d8930c2586b24
Alex Makelberge <50998628+AMakelberge@users.noreply.github.com>
2025-03-03 12:04:26 +0000

    updated requirements

--- app.py
+++ app.py
@@ -106,5 +106,5 @@ def handle_send_image(data):
         emit("maxima_code", {"status": "error", "message": str(e)})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8765))
     socketio.run(app, host="0.0.0.0", port=port, debug=True)
